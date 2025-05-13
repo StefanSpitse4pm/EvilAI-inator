4cf6a6a9ffdb85ca16a0a110cfdd2e9339fa554c
stefanspitse4pm <stefan@4propert.ie>
2025-05-13 19:14:37 +0200

    base project

---
+++ Wayfindr/app/screens/chatbot.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const Chatbot = () => {
+    return (
+    <View style={{flex: 1, alignItems:'center', justifyContent:'center'}}>
+        <Text>Chatbot</Text>
+    </View>
+    )
+}
+
+export default Chatbot;
