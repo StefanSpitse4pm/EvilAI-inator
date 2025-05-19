4e2394430dcac91d4ac80e3f2b37dc588da2ff2d
stefanspitse4pm <stefan@4propert.ie>
2025-05-19 13:34:02 +0200

    added homescreen back

---
+++ Wayfindr/app/screens/homescreen.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const Homescreen = () => {
+    return (
+    <View style={{flex: 1, alignItems:'center', justifyContent:'center'}}>
+        <Text></Text>
+    </View>
+    )
+}
+
+export default Homescreen;
