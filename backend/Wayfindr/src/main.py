12cfea68326550ed85a1764a61bb6dfedd08a5ce
stefanspitse4pm <stefan@4propert.ie>
2025-06-05 22:25:27 +0200

    Add CORS middleware to enable cross-origin requests

--- backend/Wayfindr/src/main.py
+++ backend/Wayfindr/src/main.py
@@ -2,10 +2,19 @@ from conversations.router import router as conversations_route
 from notes.router import router as notes_route
 from user.router import router as user_route
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 # project: Wayfindr
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Or your frontend's URL
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(user_route)
 app.include_router(notes_route)
