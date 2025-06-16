015622f02ab7236e65fdf1a3f90ee35f2a35c6ab
Thomas Scholtens <thomas.scholtens.scholtens@student.nhlstenden.com>
2025-06-16 19:11:29 +0200

    Refactor RootLayout component for improved readability

--- Wayfindr/app/_layout.tsx
+++ Wayfindr/app/_layout.tsx
@@ -2,9 +2,12 @@ import 'react-native-reanimated';
 import { Stack } from "expo-router";
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { StyleSheet } from 'react-native';
+import { toast } from 'sonner-native';
 
 export default function RootLayout() {
-  return <Stack screenOptions={{headerTitle:'Wayfindr',headerTitleAlign:'center', headerTitleStyle:{fontWeight:'bold', fontSize: 24}}}/>;
+  return (
+    <Stack screenOptions={{headerTitle:'Wayfindr',headerTitleAlign:'center', headerTitleStyle:{fontWeight:'bold', fontSize: 24}}}/>
+  );
 }
 
 const styles = StyleSheet.create({
