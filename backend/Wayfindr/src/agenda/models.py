7538f1b3d67ccb62ad1742a21b84ab57c551a138
stefanspitse4pm <stefan@4propert.ie>
2025-06-18 05:22:42 +0200

    Add agenda management functionality with CRUD operations

---
+++ backend/Wayfindr/src/agenda/models.py
@@ -0,0 +1,13 @@
+from sqlalchemy import  VARCHAR, DateTime, Integer, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, DeclarativeBase
+from datetime import datetime
+
+class Base(DeclarativeBase):
+    pass
+class Agenda(Base):
+    __tablename__ = "Agenda"
+    AgendaID: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
+    UserID: Mapped[int] = mapped_column(Integer, ForeignKey("User.userId"), nullable=False)
+    startTime: Mapped[datetime] = mapped_column(DateTime, nullable=False)
+    endTime: Mapped[datetime] = mapped_column(DateTime, nullable=False)
+    title: Mapped[str] = mapped_column(VARCHAR(255), nullable=False)
